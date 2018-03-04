f580e8d56c52c47ac437c21de2627986d6b5018e
Lachezar Kuzmanov <lachezar.kuzmanov@together.bg>
2018-03-04 16:09:09 +0200

    new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hiveos_autoswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
